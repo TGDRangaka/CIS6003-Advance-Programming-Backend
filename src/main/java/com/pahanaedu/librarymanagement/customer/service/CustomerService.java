91acbe16d5cae54d163ed77a122a3503d09d8405
TGDRangaka <dilshanrangaka@gmail.com>
2025-08-08 21:48:38 +0530

    feat: implemented customer service

---
+++ src/main/java/com/pahanaedu/librarymanagement/customer/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.pahanaedu.librarymanagement.customer.service;
+
+import com.pahanaedu.librarymanagement.customer.dto.CustomerDTO;
+
+import javax.servlet.ServletException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CustomerService {
+    List<CustomerDTO> getAll() throws SQLException;
+    CustomerDTO getById(String accountNumber) throws SQLException;
+    void addCustomer(CustomerDTO customer) throws SQLException;
+    void updateCustomer(CustomerDTO customer) throws SQLException;
+    void deleteCustomer(String accountNumber) throws SQLException;
+}
