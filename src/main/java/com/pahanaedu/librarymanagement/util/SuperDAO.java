bd1c7b5d3bf8905894ecc6d56de9ec129dee8e34
TGDRangaka <dilshanrangaka@gmail.com>
2025-08-09 21:14:49 +0530

    refactor: implemented super dao for simplicity

---
+++ src/main/java/com/pahanaedu/librarymanagement/util/SuperDAO.java
@@ -0,0 +1,12 @@
+package com.pahanaedu.librarymanagement.util;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface SuperDAO<T,ID> {
+    void save(T entity) throws SQLException;
+    void update(T entity) throws SQLException;
+    void delete(ID id) throws SQLException;
+    List<T> getAll() throws SQLException;
+    T getById(ID id) throws SQLException;
+}
