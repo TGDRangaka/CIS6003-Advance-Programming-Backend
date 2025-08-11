ad6ce504d02537c09b33df558cf4153a4f4c8426
TGDRangaka <dilshanrangaka@gmail.com>
2025-08-11 09:07:06 +0530

    feat: implement bill dao

---
+++ src/main/java/com/pahanaedu/librarymanagement/bill/dao/BillDAO.java
@@ -0,0 +1,7 @@
+package com.pahanaedu.librarymanagement.bill.dao;
+
+import com.pahanaedu.librarymanagement.bill.model.Bill;
+import com.pahanaedu.librarymanagement.util.SuperDAO;
+
+public interface BillDAO extends SuperDAO<Bill, String> {
+}
