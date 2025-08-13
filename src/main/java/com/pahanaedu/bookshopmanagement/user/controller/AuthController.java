f8fa3de7ef817e3404afc5ecca3256e39b56f422
TGDRangaka <dilshanrangaka@gmail.com>
2025-08-13 22:57:43 +0530

    feat: login api implemented

---
+++ src/main/java/com/pahanaedu/bookshopmanagement/user/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.pahanaedu.bookshopmanagement.user.controller;
+
+import com.pahanaedu.bookshopmanagement.user.service.UserService;
+import com.pahanaedu.bookshopmanagement.user.service.impl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "authController", value = "/auth/*")
+public class AuthController extends HttpServlet {
+    private final UserService userService;
+
+    public AuthController() {
+        userService = new UserServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
+        if (pathInfo != null && pathInfo.equals("/login")) {
+            String email = req.getParameter("email");
+            String password = req.getParameter("password");
+
+            try {
+                boolean isAuthenticated = userService.isUserExist(email, password);
+                if (isAuthenticated) {
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().write("{\"message\": \"Login successful\"}");
+                } else {
+                    resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    resp.getWriter().write("{\"error\": \"Invalid email or password\"}");
+                }
+            } catch (Exception e) {
+                resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                resp.getWriter().write("{\"error\": \"" + e.getMessage() + "\"}");
+            }
+        } else {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().write("{\"error\": \"Endpoint not found\"}");
+        }
+    }
+}
